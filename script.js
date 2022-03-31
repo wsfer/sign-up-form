90d22027d33ec932d18156613bce9abcdfd36db4
wesley <wesley_snaine@hotmail.com>
2022-03-31 17:41:00 -0300

    create a script to mark warn about 'password confirm' as invalid

--- script.js
+++ script.js
@@ -15,5 +15,21 @@ function changeTheme () {
 
 //Form validation scripts.
 let firstName = document.querySelector('#first-name');
+let lastName = document.querySelector('#last-name');
+let email = document.querySelector('#email');
+let phone = document.querySelector('#phone');
 let password = document.querySelector('#password');
 let passwordConfirm = document.querySelector('#psw-confirm');
+let pswErrorMsg = document.querySelector('.psw-error');
+
+passwordConfirm.addEventListener('focusout', checkPassword);
+
+function checkPassword () {
+    if (!passwordConfirm.value || password.value == passwordConfirm.value) {
+        pswErrorMsg.style.display = "none";
+        passwordConfirm.classList.remove('invalid-password');
+    } else {
+        pswErrorMsg.style.display = "block";
+        passwordConfirm.classList.add('invalid-password');
+    }
+}
